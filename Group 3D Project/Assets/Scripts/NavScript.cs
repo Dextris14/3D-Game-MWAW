819a65674e7136e08504bcf0e4fa5eb3f12a0b9a
Alex Wise <19.alex.wise@ksd.org>
2019-04-23 08:25:23 -0700

    Added nav mesh, and made basic following enemy

---
+++ Group 3D Project/Assets/Scripts/NavScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent(typeof(NavMeshAgent))]
+public class NavScript : MonoBehaviour
+{
+    NavMeshAgent Agent;
+    GameObject Player;
+    public float ChaseDistance = 10;
+    private Vector3 Home;
+
+    void Start()
+    {
+        Home = transform.position;
+        Agent = GetComponent<NavMeshAgent>();
+        Player = GameObject.Find("RigidBodyFPSController");
+    }
+
+    void Update()
+    {
+        Vector3 Direction = Player.transform.position - transform.position;
+        if (Direction.magnitude <= ChaseDistance)
+        {
+            Agent.destination = Player.transform.position;
+        }
+        else
+        {
+            Agent.destination = Home;
+        }
+    }
+}
