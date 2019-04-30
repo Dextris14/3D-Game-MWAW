4a2c22eed3cefee34963fb22fd399323095b2ef2
Alex Wise <19.alex.wise@ksd.org>
2019-04-30 08:22:40 -0700

    Updated firebolt

---
+++ Group 3D Project/Assets/Scripts/Firebolt.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Firebolt : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<Rigidbody>().velocity = (GameObject.Find("RigidBodyFPSController").transform.position - transform.position).normalized * 10;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.layer == 10)
+        {
+
+        }
+    }
+}
