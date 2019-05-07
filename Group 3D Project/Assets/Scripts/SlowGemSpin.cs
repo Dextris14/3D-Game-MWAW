626f0b1dce506d1c10d0cff3deeffa82e1c2efa2
Todd Schueren <19.todd.schueren@ksd.org>
2019-05-07 09:53:07 -0700

    more models and textures. almost finished start menu

---
+++ Group 3D Project/Assets/Scripts/SlowGemSpin.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class SlowGemSpin : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GetComponent<Transform>().Rotate(new Vector3(0, 1, 0));
+    }
+}
